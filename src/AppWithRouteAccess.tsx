9e860b86ad6d6a8ffa12ce65fc883b7f79aab1c6
Balaji Pavan Kumar <101182094+balaji511@users.noreply.github.com>
2024-03-18 21:38:38 +0530

    file setup 3

---
+++ src/AppWithRouteAccess.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const AppWithRouteAccess: React.FC = () => {
+  return (
+    <div>
+      <Typography>App Component</Typography>
+    </div>
+  );
+};
+
+export default AppWithRouteAccess;
